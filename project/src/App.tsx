3b59db73d76ab31e99a74b0dfc6115531be8283d
bartsobrinho <bartsobrinho@gmail.com>
2025-04-06 17:41:06 -0300

    chore: restore main branch files

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,42 +1,58 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { 
+  Routes, 
+  Route, 
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
-import { Content } from './pages/Content';
+import { ContentPage } from './pages/Content';
 import { Activities } from './pages/Activities';
 import { Resources } from './pages/Resources';
 import { Admin } from './pages/Admin';
 import { UploadContent } from './pages/UploadContent';
-import Analytics from './pages/Analytics';
-import PersonalizedRecommendations from './components/PersonalizedRecommendations';
-import { AuthProvider } from './contexts/AuthContext';
 
-const App: React.FC = () => {
+// Layout component that includes Header and Footer
+const Layout = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/content" element={<Content />} />
-              <Route path="/activities" element={<Activities />} />
-              <Route path="/resources" element={<Resources />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/upload" element={<UploadContent />} />
-              <Route path="/analytics" element={<Analytics />} />
-              <Route path="/recommendations" element={<PersonalizedRecommendations userId="current" />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </AuthProvider>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 };
+
+// Configuração do router com as flags de futuro
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/content" element={<ContentPage />} />
+      <Route path="/activities" element={<Activities />} />
+      <Route path="/resources" element={<Resources />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/upload" element={<UploadContent />} />
+    </Route>
+  ),
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true
+    }
+  }
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
